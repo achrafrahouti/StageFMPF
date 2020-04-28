602b1f7aa14635b5ee2cb99067cf234a0c2219b9
ACHRAF_RAHOUTI <achraf.rahouti@usmba.ac.ma>
2020-04-28 15:38:25 +0100

    commited By Achraf RAhouti

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::redirect('/', '/login');
+
+Route::redirect('/home', '/admin');
+
+Auth::routes(['register' => false]);
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+
+    Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
+
+    Route::resource('permissions', 'PermissionsController');
+
+    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
+
+    Route::resource('roles', 'RolesController');
+
+    Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
+
+    Route::resource('users', 'UsersController');
+
+    Route::delete('products/destroy', 'ProductsController@massDestroy')->name('products.massDestroy');
+
+    Route::resource('products', 'ProductsController');
+});
