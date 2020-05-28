45a7b6dc58970df2495268193953867c246eabe2
ACHRAF_RAHOUTI <achraf.rahouti@usmba.ac.ma>
2020-05-28 22:06:07 +0100

    Update Groupe

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -52,7 +52,7 @@ class UsersTableSeeder extends Seeder
             'email'          => 'encadrant@encadrant.com',
             'password'       => Hash::make('password'),
             'profile_type'   =>'App\Encadrant',
-            'profile_id'     =>null,
+            'profile_id'     =>'1',
             'remember_token' => null,
             'created_at'     => '2019-04-15 19:13:32',
             'updated_at'     => '2019-04-15 19:13:32',
